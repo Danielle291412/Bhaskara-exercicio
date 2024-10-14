a5bc02db0cbef821f217da4ac4172b9e0d3a3aa2
Danielle Gouveia <danielle.vieira9@etec.sp.gov.br>
2024-10-14 19:48:20 -0300

    mostrar o calculo das raizes de segundo grau

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+double a, b, c, contagem, x1, x2;
+
+Console.WriteLine("Equação de Segundo Grau");
+Console.Write("(a):");
+a = Convert.ToDouble (Console.ReadLine());
+Console.Write("(b):");
+b = Convert.ToDouble (Console.ReadLine());
+Console.Write("(c):");
+c = Convert.ToDouble (Console.ReadLine());
+
+
+if (a == 0)
+
+{
+    Console.WriteLine("Não é uma equação do segundo grau");
+}
+else 
+{
+    contagem = b * b - 4 * a * c;
+
+    if (contagem < 0)
+    {
+        Console.WriteLine($"Como delta = {contagem:N2}, a equação não obtem raizes reais");
+    }
+    else
+    {
+        x1 = (-b + Math.Sqrt(contagem)) / (2 * a);
+        x2 = (-b - Math.Sqrt(contagem)) / (2 * a);
+
+        Console.WriteLine($"x1 = {x1:N2} e x2 + {x2:N2}");
+    }
+}
